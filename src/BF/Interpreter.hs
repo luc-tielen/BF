a30512e38fc7f5e5d8d00a2692c4832c33afd109
Luc Tielen <luc.tielen@icrealisations.com>
2017-04-01 01:03:28 +0200

    Minor bugfix in interpreter code

--- src/BF/Interpreter.hs
+++ src/BF/Interpreter.hs
@@ -5,11 +5,11 @@ module BF.Interpreter ( interpret
 import Control.Monad.State
 import Data.Array.Unboxed hiding (array)
 import qualified Data.Array.IArray as A
-import Data.Char
+import Data.Word
 import BF.Types
 
 type Index = Int
-type Byte = Char
+type Byte = Word8
 type ByteArray = UArray Index Byte
 data Interpreter = Interpreter {
   idx :: Index,
@@ -25,22 +25,23 @@ evalInstruction :: Instruction -> InterpreterStateT
 evalInstruction IncrementPtr = modify $ \s@(Interpreter i _) -> s { idx = i + 1 }
 evalInstruction DecrementPtr = modify $ \s@(Interpreter i _) -> s { idx = i - 1 }
 evalInstruction IncrementValue = modify $ \s@(Interpreter i a) -> s { array = increment i a }
-  where increment i a = ixmap (i, i) (\v -> v + 1) a
+  where increment i a = a // [(i, (a ! i) + 1)]  --ixmap (i, i) (\v -> v + 1) a
 evalInstruction DecrementValue = modify $ \s@(Interpreter i a) -> s { array = decrement i a }
-  where decrement i a = ixmap (i, i) (\v -> v - 1) a
+  where decrement i a = a // [(i, (a ! i) - 1)]
 evalInstruction WriteOutput = do
   Interpreter i a <- get
   let v = a ! i
-  liftIO $ putChar v
+  liftIO $ putChar . toEnum . fromIntegral $ v
 evalInstruction ReadInput = do
   s@(Interpreter i a1) <- get
   input <- liftIO $ getChar
-  let a2 = a1 // [(i, input)]
+  let input' = fromIntegral . fromEnum $ input
+  let a2 = a1 // [(i, input')]
   put s { array = a2 }
 evalInstruction loop@(Loop instructions) = do
   Interpreter i a <- get
   let v = a ! i
-  case ord v of
+  case v of
     0 -> return ()
     _ -> evalInstructions instructions >> evalInstruction loop
 
@@ -48,7 +49,8 @@ evalInstructions :: [Instruction] -> InterpreterStateT
 evalInstructions = mapM_ evalInstruction
 
 interpret :: [Instruction] -> IO ()
-interpret instructions = evalStateT' interpreter $ evalInstructions instructions
+interpret instructions = do
+  evalStateT' interpreter $ evalInstructions instructions
   where interpreter = Interpreter 0 makeArray
-        makeArray = A.array (0, lenArray - 1) [(i, chr 0) | i <- [0, lenArray - 1]]
+        makeArray = A.array (0, lenArray - 1) [(i, 0) | i <- [0 .. lenArray - 1]]
         lenArray = 30000
