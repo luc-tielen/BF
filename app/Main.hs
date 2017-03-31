f1f9617662972711530d7c73e84d66b029806240
Luc Tielen <luc.tielen@icrealisations.com>
2017-04-01 01:13:16 +0200

    Use argv to execute a file

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,20 @@ module Main where
 
 import Lib
 import qualified Text.Megaparsec as P
+import System.Environment
 
 
 main :: IO ()
 main = do
-  let filename = "test.bf"
+  args <- getArgs
+  handleArgs args
+
+handleArgs :: [String] -> IO ()
+handleArgs (filename:_) = do
   programString <- readFile filename
   case P.parse parse filename programString of
     Left e -> print e
     Right instructions -> interpret instructions
+handleArgs _ = do
+  progName <- getProgName
+  print $ "Usage: " ++ progName ++ " FILENAME"
